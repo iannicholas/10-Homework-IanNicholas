47f754a71272a260e821cbece55280fb81ebd3ce
CodeNinja <inicholas8686@gmail.com>
2022-02-22 17:29:57 -0800

    working test files

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,28 +1,28 @@
-const Employee = require("./lib/Employee");
+const Employee = require("../lib/Employee");
 
 test ("creates an employee object", () => {
   const employee = new Employee("Ian Nicholas", 31789, "inicholas8686@gmail.com");
   expect(employee.name).toEqual(expect.any(String));
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
-})
+});
 
 test ("gets employee name", () => {
   const employee = new Employee("Ian Nicholas", 31789, "inicholas8686@gmail.com");
   expect(employee.getName()).toEqual(expect.any(String));
-})
+});
 
 test ("gets employee id", () => {
   const employee = new Employee("Ian Nicholas", 31789, "inicholas8686@gmail.com");
   expect(employee.getId()).toEqual(expect.any(Number));
-})
+});
 
 test ("gets employee email", () => {
   const employee = new Employee("Ian Nicholas", 31789, "inicholas8686@gmail.com");
   expect(employee.getEmail()).toEqual(expect.any(String));
-})
+});
 
 test ("gets employee status", () => {
   const employee = new Employee("Ian Nicholas", 31789, "inicholas8686@gmail.com");
-  expect(employee.getStatus()).toEqual(expect.any("Employee"));
+  expect(employee.getStatus()).toEqual("Employee");
 });
--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,16 +1,16 @@
-const Intern = require("./lib/Intern");
+const Intern = require("../lib/Intern");
 
 test ("creates an intern object", () => {
   const intern = new Intern("Ian Nicholas", 31789, "inicholas8686@gmail.com", "UCSD");
-  expect(intern.getGithub()).toEqual(expect.any(String));
-})
+  expect(intern.school).toEqual(expect.any(String));
+});
 
 test ("gets intern school", () => {
   const intern = new Intern("Ian Nicholas", 31789, "inicholas8686@gmail.com", "UCSD");
-  expect(intern.getGithub()).toEqual(expect.any(String));
-})
+  expect(intern.getSchool()).toEqual(expect.any(String));
+});
 
 test ("gets intern status", () => {
   const intern = new Intern("Ian Nicholas", 31789, "inicholas8686@gmail.com", "UCSD");
-  expect(intern.getStatus()).toEqual(expect.any("Intern"));
+  expect(intern.getStatus()).toEqual("Intern");
 });
